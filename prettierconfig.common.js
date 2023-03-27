0fb1bebad4f98eafe4e50c863097f592fc93a34b
Alexandr Petyaykin <saneksa2@gmail.com>
2023-03-27 09:56:39 +0300

    feat: добавлен конфиг для prettier

---
+++ prettierconfig.common.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import("prettier").Options}
+ */
+module.exports = {
+  printWidth: 100,
+  endOfLine: "lf",
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  rangeEnd: Infinity,
+  rangeStart: 0,
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+};
